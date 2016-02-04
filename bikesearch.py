94b4aeac107fa814cb5dbfe7db71795f239c905d
Dejvo <potion.potion@gmail.com>
2016-02-04 11:03:55 +0100

    Update and rename scraper.py to bikesearch.py

--- bikesearch.py
+++ bikesearch.py
@@ -1,27 +1,5 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
-
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
-
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+#!/usr/bin/env python
+# coding=utf8
 
 import cgitb
 import urllib2
@@ -30,8 +8,8 @@ import sys
 from lxml import html
 
 #print sys.argv[1]
-#bikesearch = sys.argv[1] 
-bikesearch = 'mongoose'
+bikesearch = sys.argv[1] 
+#bikesearch = 'mongoose'
 
 print "Search for: " + bikesearch + "<hr>"
 
