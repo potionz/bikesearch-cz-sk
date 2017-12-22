2a516787f2ba0a027ea989024cb464717f56a814
potionz <potion@synapsia.org>
2017-12-22 16:59:28 +0100

    add search text input, simplify some code

--- index.php
+++ index.php
@@ -10,7 +10,9 @@
 <br>
 
   Search for bike on cz/sk bike bazars<br>
+  <input type="radio" name="search" value=""><input type="text" name="search" id="searchid" value=""><br>
   <input type="radio" name="search" value="enduro"> Search for enduro.<br>
+  <input type="radio" name="search" value="bighit"> Search for BigHit.<br>
   <input type="radio" name="search" value="YT"> Search for YT.<br>
   <input type="radio" name="search" value="wicked"> Search for wicked.<br>
   <input type="radio" name="search" value="trek"> Search for Trek.<br>
@@ -47,71 +49,89 @@ $file = 'searches';
 
 
 if (isset($_POST['submit'])) {
- if ($_POST['search']=="enduro") {
-  $command = "python bikesearch.py enduro 2>&1";
- }
- if ($_POST['search']=="YT") {
-  $command = "python bikesearch.py YT 2>&1";
- }
- if ($_POST['search']=="wicked") {
-  $command = "python bikesearch.py wicked 2>&1";
- }  
- if ($_POST['search']=="trek") {
-  $command = "python bikesearch.py trek 2>&1";
- }    
- if ($_POST['search']=="norco") {
-  $command = "python bikesearch.py norco 2>&1";
- }   
- if ($_POST['search']=="nsbike") {
-  $command = "python bikesearch.py ns+bike 2>&1";
- }
- if ($_POST['search']=="devilwork") {
-  $command = "python bikesearch.py devilwork 2>&1";
- }
- if ($_POST['search']=="duratec") {
-  $command = "python bikesearch.py duratec 2>&1";
- }       
- if ($_POST['search']=="onone") {
-  $command = "python bikesearch.py on-one 2>&1";
- }
- if ($_POST['search']=="mongoose") {
-  $command = "python bikesearch.py mongoose 2>&1";
- }
- if ($_POST['search']=="dartmoor") {
-  $command = "python bikesearch.py dartmoor 2>&1";
- }   
- if ($_POST['search']=="ragley") {
-  $command = "python bikesearch.py ragley 2>&1";
- }
- if ($_POST['search']=="banshee") {
-  $command = "python bikesearch.py banshee  2>&1";
- }
- if ($_POST['search']=="caprnka") {
-  $command = "python bikesearch.py caprnka 2>&1";
- }
  if ($_POST['search']=="charge") {
    print "Ak si nasiel <b>charge blender</b> niekde na predaj, prosim kontaktuj mna na <b>bike@synapsia.org</b><br>";
-   print "If you find black charge blender with bomber fork somewhere on net, please contact me on bike@synapsia.org<br><br><hr>"; 
+   print "If you find black charge blender with bomber fork somewhere on net, please contact me on bike@synapsia.org<br><br><hr>";
    $command = "python bikesearch.py charge 2>&1";
    # Ak si nasiel charge blender niekde na predaj, prosim kontaktuj mna na bike@synapsia.sk
    # If you find black charge blender with bomber fork somewhere on net, please contact me on bike@synapsia.sk
 # $command = "python bikesearch.py charge 2>&1";
  }
- if ($_POST['search']=="mrazek") {
-  $command = "python bikesearch.py mrazek 2>&1";
+ else {
+   $command = "python bikesearch.py ".$_POST['search']." 2>&1";
  }
- if ($_POST['search']=="franta") {
-  $command = "python bikesearch.py franta 2>&1";
- }
- if ($_POST['search']=="nukeproof") {
-  $command = "python bikesearch.py nukeproof 2>&1";
- }
- if ($_POST['search']=="commencal") {
-  $command = "python bikesearch.py commencal 2>&1";
- }
- if ($_POST['search']=="agang") {
-  $command = "python bikesearch.py agang 2>&1";
- }
+
+ 
+
+# $command = "python bikesearch.py ".$_POST['search']." 2>&1";
+# if ($_POST['search']=="enduro") {
+#  $command = "python bikesearch.py enduro 2>&1";
+# }
+# if ($_POST['search']=="bighit") {
+#  $command = "python bikesearch.py bighit 2>&1";
+# }
+# if ($_POST['search']=="YT") {
+#  $command = "python bikesearch.py YT 2>&1";
+# }
+# if ($_POST['search']=="wicked") {
+#  $command = "python bikesearch.py wicked 2>&1";
+# }  
+# if ($_POST['search']=="trek") {
+#  $command = "python bikesearch.py trek 2>&1";
+# }    
+# if ($_POST['search']=="norco") {
+#  $command = "python bikesearch.py norco 2>&1";
+# }   
+# if ($_POST['search']=="nsbike") {
+#  $command = "python bikesearch.py ns+bike 2>&1";
+# }
+# if ($_POST['search']=="devilwork") {
+#  $command = "python bikesearch.py devilwork 2>&1";
+# }
+# if ($_POST['search']=="duratec") {
+#  $command = "python bikesearch.py duratec 2>&1";
+# }       
+# if ($_POST['search']=="onone") {
+#  $command = "python bikesearch.py on-one 2>&1";
+# }
+# if ($_POST['search']=="mongoose") {
+#  $command = "python bikesearch.py mongoose 2>&1";
+# }
+# if ($_POST['search']=="dartmoor") {
+#  $command = "python bikesearch.py dartmoor 2>&1";
+# }   
+# if ($_POST['search']=="ragley") {
+#  $command = "python bikesearch.py ragley 2>&1";
+# }
+# if ($_POST['search']=="banshee") {
+#  $command = "python bikesearch.py banshee  2>&1";
+# }
+# if ($_POST['search']=="caprnka") {
+#  $command = "python bikesearch.py caprnka 2>&1";
+# }
+# if ($_POST['search']=="charge") {
+#   print "Ak si nasiel <b>charge blender</b> niekde na predaj, prosim kontaktuj mna na <b>bike@synapsia.org</b><br>";
+#   print "If you find black charge blender with bomber fork somewhere on net, please contact me on bike@synapsia.org<br><br><hr>"; 
+#   $command = "python bikesearch.py charge 2>&1";
+#   # Ak si nasiel charge blender niekde na predaj, prosim kontaktuj mna na bike@synapsia.sk
+#   # If you find black charge blender with bomber fork somewhere on net, please contact me on bike@synapsia.sk
+## $command = "python bikesearch.py charge 2>&1";
+# }
+# if ($_POST['search']=="mrazek") {
+#  $command = "python bikesearch.py mrazek 2>&1";
+# }
+# if ($_POST['search']=="franta") {
+#  $command = "python bikesearch.py franta 2>&1";
+# }
+# if ($_POST['search']=="nukeproof") {
+#  $command = "python bikesearch.py nukeproof 2>&1";
+# }
+# if ($_POST['search']=="commencal") {
+#  $command = "python bikesearch.py commencal 2>&1";
+# }
+# if ($_POST['search']=="agang") {
+#  $command = "python bikesearch.py agang 2>&1";
+# }
  
 
 
